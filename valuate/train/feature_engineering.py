57454b3245f85619670630c9ffb26f4e1459d82a
leidengjun <m18349125880@gmail.com>
2018-12-05 23:03:21 +0800

    mod data_analyze

--- valuate/train/feature_engineering.py
+++ valuate/train/feature_engineering.py
@@ -84,27 +84,8 @@ def adjust_k(df):
 class FeatureEngineering(object):
 
     def __init__(self):
-        # self.train = pd.read_csv(path + '../tmp/train/car_source.csv')
+        self.train = pd.read_csv(path + '../tmp/train/car_source_match.csv')
         self.province_city_map = pd.read_csv(path + 'predict/map/province_city_map.csv')
-        # self.open_category = pd.read_csv(path + '../tmp/train/open_category.csv')
-        # brand = self.open_category.loc[(self.open_category['parent'].isnull()), :]
-        # brand = brand.loc[:, ['name', 'slug', 'brand_area']].rename(
-        #     columns={'name': 'brand_name', 'slug': 'brand_slug'})
-        #
-        # self.open_category = self.open_category.loc[(self.open_category['parent'].notnull()), :]
-        # self.open_category = self.open_category.rename(columns={'slug': 'global_slug', 'parent': 'brand_slug', 'name': 'global_name'})
-        # self.open_category = self.open_category.loc[:, ['brand_slug', 'global_slug', 'global_name']]
-        # self.open_category = self.open_category.merge(brand, how='left', on=['brand_slug'])
-        # self.open_model_detail = pd.read_csv(path + '../tmp/train/open_model_detail.csv')
-        # self.open_model_detail = self.open_model_detail.merge(self.open_category, how='left', on=['global_slug'])
-        # self.open_model_detail = self.open_model_detail.loc[(self.open_model_detail['brand_area'].notnull()), :].reset_index(drop=True)
-        # self.open_model_detail = self.open_model_detail.rename(columns={'year': 'online_year'})
-        # self.model_global_mean = self.open_model_detail.loc[:,
-        #                     ['brand_name', 'global_name', 'detail_model', 'brand_slug', 'global_slug', 'model_detail_slug', 'online_year', 'price_bn',
-        #                      'volume', 'control', 'emission_standard', 'brand_area']]
-        # self.open_model_detail = self.open_model_detail.loc[:,
-        #                     ['brand_name', 'global_name', 'detail_model', 'model_detail_slug', 'online_year',
-        #                      'volume', 'control', 'emission_standard', 'brand_area']]
 
     ###########################
     # 数据清洗
@@ -113,10 +94,6 @@ class FeatureEngineering(object):
         """
         处理数据质量
         """
-        self.train = self.train.loc[(self.train['domain'].isin(['guazi.com', 'renrenche.com'])) & (self.train['model_detail_slug'].notnull()), :]
-        self.train.reset_index(inplace=True, drop=True)
-        self.train = self.train.merge(self.open_model_detail, how='left', on=['model_detail_slug'])
-        self.train = self.train.drop(['popularity', 'expired_at', 'sold_time', 'source_type', 'status', 'domain'], axis=1)
         # 删掉price<0的记录
         self.train = self.train[self.train['price'] > 0]
         self.train = self.train[self.train['price_bn'] > 0]
@@ -124,10 +101,8 @@ class FeatureEngineering(object):
         self.train = self.train[(self.train['year'] <= datetime.datetime.now().year)]
         self.train = self.train[self.train['month'].isin(list(np.arange(1, 13)))]
         # 删除掉未知城市,未知款型的记录
-        cities = list(set(self.province_city_map.city.values))
-        models = list(set(self.open_model_detail.model_detail_slug.values))
-        self.train = self.train[self.train['city'].isin(cities)]
-        self.train = self.train[self.train['model_detail_slug'].isin(models)]
+        # cities = list(set(self.province_city_map.city.values))
+        # self.train = self.train[self.train['city'].isin(cities)]
         self.train.reset_index(inplace=True, drop='index')
 
     def handle_data_preprocess(self):
@@ -136,20 +111,20 @@ class FeatureEngineering(object):
         1.删除离群点
         """
         # 删除数据量小于12的款型
-        detail_num = self.train.groupby(['model_detail_slug'])['detail_model'].count().reset_index()
-        detail_num = detail_num.loc[(detail_num['detail_model'] >= 12), :]
-        self.train = self.train.loc[(self.train['model_detail_slug'].isin(list(set(detail_num.model_detail_slug.values)))), :]
+        detail_num = self.train.groupby(['detail_slug'])['detail_name'].count().reset_index()
+        detail_num = detail_num.loc[(detail_num['detail_name'] >= 12), :]
+        self.train = self.train.loc[(self.train['detail_slug'].isin(list(set(detail_num.detail_slug.values)))), :]
         self.train.reset_index(inplace=True, drop=True)
 
         # 根据款型计算均值
-        mean_value = self.train.groupby(['model_detail_slug'])['price'].mean().reset_index().rename(
+        mean_value = self.train.groupby(['detail_slug'])['price'].mean().reset_index().rename(
             columns={'price': 'mean_value'})
-        self.train = self.train.merge(mean_value, how='left', on=['model_detail_slug'])
+        self.train = self.train.merge(mean_value, how='left', on=['detail_slug'])
 
         # 根据款型计算标准差
-        std_value = self.train.groupby(['model_detail_slug'])['price'].std().reset_index().rename(
+        std_value = self.train.groupby(['detail_slug'])['price'].std().reset_index().rename(
             columns={'price': 'std_value'})
-        self.train = self.train.merge(std_value, how='left', on=['model_detail_slug'])
+        self.train = self.train.merge(std_value, how='left', on=['detail_slug'])
 
         # 计算Z得分，根据阈值删除离群点
         self.train['z_score'] = self.train.apply(cal_z_score, axis=1)
@@ -381,9 +356,9 @@ class FeatureEngineering(object):
         """
         执行
         """
-        # self.handle_data_quality()
-        # self.handle_data_preprocess()
-        # self.generate_model_global_mean()
+        self.handle_data_quality()
+        self.handle_data_preprocess()
+        self.generate_model_global_mean()
         self.generate_price_bn_div_map()
         # self.generate_province_div_map()
         # self.generate_warehouse_years_div_map()
